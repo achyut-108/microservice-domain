c196472188765ad0f925edb0181e6bf2e12089c0
user <user@user-PC>
2021-02-25 22:08:51 +0530

    eureka

---
+++ eureka-server/src/test/java/com/ihs/gsg/eurekaserver/starter/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihs.gsg.eurekaserver.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
